5e499662a98c3a91188b7d677a8ccb40f1e5b44b
Arnau Costa <arnaucosta95@gmail.com>
2024-12-18 14:57:39 +0100

    change v3

--- LaKultural/settings.py
+++ LaKultural/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = "django-insecure-pbym&s()se0#%+pw!c7n)ngsepx=re$^jg-(pte+l6v%c96r+)"
+SECRET_KEY = "django-insecure-pbym&s()se0#%+pw!c7n)ngsepx=re$^jg-(pte+l6v%c96r+)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
