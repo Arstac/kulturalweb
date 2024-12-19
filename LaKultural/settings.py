823386f2fec5e3ed461cf10afa1bbbc951699939
Arnau Costa <arnaucosta95@gmail.com>
2024-12-19 15:34:22 +0100

    settings chg media

--- LaKultural/settings.py
+++ LaKultural/settings.py
@@ -168,7 +168,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
