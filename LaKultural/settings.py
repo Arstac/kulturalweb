f370489fa35b2530420b342efbe3f6c77df40313
Arnau Costa <arnaucosta95@gmail.com>
2024-12-19 14:05:07 +0100

    db settings

--- LaKultural/settings.py
+++ LaKultural/settings.py
@@ -110,12 +110,17 @@ if ENVIRONMENT == 'production':
     }
 else:  # Configuración local para desarrollo
     DEBUG = True
+    
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'railway',
+            'USER': 'postgres',
+            'PASSWORD': 'bTohQDfQUHQSrtPIttEFNaeEVHKVQtFq',
+            'HOST': 'autorack.proxy.rlwy.net',
+            'PORT': '22209',
+            }
         }
-    }
     
 
 # Password validation
