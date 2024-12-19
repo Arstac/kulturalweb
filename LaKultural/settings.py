13bdf548a3c376286f20186056c22001fae7520b
Arnau Costa <arnaucosta95@gmail.com>
2024-12-19 16:04:55 +0100

    settings

--- LaKultural/settings.py
+++ LaKultural/settings.py
@@ -169,7 +169,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 #MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_ROOT = '/media'
+MEDIA_ROOT = '/media_data/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
