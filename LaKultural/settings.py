b34a5a843877a0a131d3f10007f3381f147ac392
Arnau Costa <arnaucosta95@gmail.com>
2025-04-03 15:49:06 +0200

    pro digitalocean

--- LaKultural/settings.py
+++ LaKultural/settings.py
@@ -8,11 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pbym&s()se0#%+pw!c7n)ngsepx=re$^jg-(pte+l6v%c96r+)"
-
+SECRET_KEY = os.getenv("SECRET_KEY", "clave-insegura-dev")
 # Para poder registrar usuarios en producción
-CSRF_TRUSTED_ORIGINS = ['https://kulturalweb-production.up.railway.app', 'https://kulturalweb-development.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['*']
 
 
 # Application definition
@@ -25,9 +23,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'django_jsonforms',
-    "whitenoise.runserver_nostatic",
-    'cloudinary',
-    'cloudinary_storage',
     'biografia',
     'booking',
     'eventos',
@@ -41,7 +36,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -74,56 +68,24 @@ WSGI_APPLICATION = "LaKultural.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# ORIGINAL
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-# Set default values for the environment variables if they’re not already set
-# os.environ.setdefault("PGDATABASE", "lakultural_dev")
-# os.environ.setdefault("PGUSER", "arnau")
-# os.environ.setdefault("PGPASSWORD", "password")
-# os.environ.setdefault("PGHOST", "localhost")
-# os.environ.setdefault("PGPORT", "5432")
-
 # Modo de entorno: 'development' o 'production'
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
 # SECURITY WARNING: update this when you have the production host
 ALLOWED_HOSTS = ['*']
 
-# Configuración de base de datos
-if ENVIRONMENT == 'production':
-    # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = True
-    
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ["PGDATABASE"],
-            'USER': os.environ["PGUSER"],
-            'PASSWORD': os.environ["PGPASSWORD"],
-            'HOST': os.environ["PGHOST"],
-            'PORT': os.environ["PGPORT"],
-        }
+DEBUG = ENVIRONMENT != 'production'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("PGDATABASE", "lakultural"),
+        'USER': os.environ.get("PGUSER", "lakultural_user"),
+        'PASSWORD': os.environ.get("PGPASSWORD", ""),
+        'HOST': os.environ.get("PGHOST", "localhost"),
+        'PORT': os.environ.get("PGPORT", "5432"),
     }
-else:  # Configuración local para desarrollo
-    DEBUG = True
-    
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'railway',
-            'USER': 'postgres',
-            'PASSWORD': 'bTohQDfQUHQSrtPIttEFNaeEVHKVQtFq',
-            'HOST': 'autorack.proxy.rlwy.net',
-            'PORT': '22209',
-            }
-        }
-    
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -162,23 +124,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-#Per un entorn de produccio
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CLOUDINARY_URL = os.getenv('CLOUDINARY_URL', 'cloudinary://725997334747238:ZHj-pnHBqLFxvEKG5Svhx46w5wM@dyhcaqlhv')
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-MEDIA_URL = '/media/'  # Esto es opcional; Cloudinary generará URLs automáticas
-MEDIA_ROOT = ''
 
 AUTHENTICATION_BACKENDS = [
     # La ruta a tu backend personalizado
