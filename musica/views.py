a3709a011169c3dc4bc0bded06463628d831dbce
Arnau Costa <arnaucosta95@gmail.com>
2025-04-04 16:10:37 +0200

    fix

--- musica/views.py
+++ musica/views.py
@@ -1,1 +1,18 @@
-v
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from django.templatetags.static import static
+from .models import Cancion
+
+def musica(request):
+    canciones = Cancion.objects.filter(visible=True)  # Solo artistas visibles
+    contexto = {'canciones': canciones}
+    return render(request, 'musica/musica.html', contexto)
+
+def cancion_detalle(request, cancion_id):
+    cancion = get_object_or_404(Cancion, id=cancion_id)
+    data = {
+        'titulo': cancion.titulo,
+        'artista': cancion.artista.nombre,
+        'imagen': request.build_absolute_uri(cancion.album.portada.url) if cancion.album else static('assets/img/default_album.png')
+    }
+    return JsonResponse(data)
